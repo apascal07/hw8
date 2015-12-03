48c5f3ceb59f25445524bb17cfc294ff79218b92
Alexandru Pascal <Alex@Alexandrus-MacBook-Pro.local>
2015-12-02 23:29:06 -0500

    Clean up

--- prefix_sum.cu
+++ prefix_sum.cu
@@ -18,97 +18,56 @@
 * You should implement the simple scan function here!
 */
 __global__ void scan_simple(float *g_odata, float *g_idata, int n) {
-extern  __shared__  float x[];
-
-// STUDENT: YOUR CODE GOES HERE.
-
-/* Pseudo-code
-1: for d = 1 to log2 n do
-2:     for all k in parallel do
-3:         if k >= 2^d  then
-4:             x[out][k] = x[in][k – 2^(d-1)] + x[in][k]
-5:         else
-6:             x[out][k] = x[in][k]
-*/
-
-g_odata[threadIdx.x] = 0.0;
-
-
-int thread_id = threadIdx.x;
-int pout = 0, pin = 1;
-// Load input into shared memory.
-// This is exclusive scan, so shift right by one
-// and set first element to 0
-if (thread_id > 0) {
-x[pout*n + thread_id] = g_idata[thread_id-1];
-}
-else {
-x[pout*n + thread_id] = 0;
-}
-__syncthreads();
-for (int offset = 1; offset < n; offset *= 2) {
-pout = 1 - pout; // swap double buffer indices
-pin = 1 - pout;
-if (thread_id >= offset) {
-x[pout*n+thread_id] = x[pin*n+thread_id - offset] + x[pin*n+thread_id];
-}
-else {
-x[pout*n+thread_id] = x[pin*n+thread_id];
-}
-__syncthreads();
-}
-g_odata[thread_id] = x[pout*n+thread_id]; // write output
-
+    extern  __shared__  float x[];
+    g_odata[threadIdx.x] = 0.0;
+    int thread_id = threadIdx.x;
+    int pout = 0, pin = 1;
+    x[pout * n + thread_id] = thread_id > 0 ? g_idata[thread_id - 1] : 0;
+    __syncthreads();
+    for (int offset = 1; offset < n; offset *= 2) {
+        pout = 1 - pout;
+        pin = 1 - pout;
+        x[pout * n + thread_id] = thread_id >= offset ? x[pin * n + thread_id - offset] + x[pin * n + thread_id] : x[pin * n + thread_id];
+        __syncthreads();
+    }
+    g_odata[thread_id] = x[pout * n + thread_id];
 }
 
 /*
 * You should implement the prescan kernel function here!
 */
 __global__ void prescan(float *g_odata, float *g_idata, int n) {
-extern  __shared__  float x[];
-
-// STUDENT: YOUR CODE GOES HERE.
-/* Pseudo-code
-1: for d = 0 to log2 n – 1 do
-2:     for all k = 0 to n – 1 by 2 d+1 in parallel do
-3:         x[k +  2^(d+1) – 1] = x[k +  2^d – 1] + x[k +  2^(d+1) – 1]
-4:		   x[n – 1] <-- 0
-5:		   for d = log2 n – 1 down to 0 do
-6:             for all k = 0 to n – 1 by 2 d +1 in parallel do
-7:             t = x[k +  2^d  – 1]
-8:             x[k +  2^d – 1] = x[k +  2^(d+1) – 1]
-9:             x[k +  2^(d+1) – 1] = t +  x[k +  2^(d+1) – 1]
-*/
-int thread_id = threadIdx.x;
-int offset = 1;
-x[2*thread_id] = g_idata[2*thread_id]; // load input into shared memory
-x[2*thread_id+1] = g_idata[2*thread_id+1];
-for (int d_reduction = n>>1; d_reduction > 0; d_reduction >>= 1) {  // build sum in place up the tree
-__syncthreads();
-if (thread_id < d_reduction) {
-int ai = offset*(2*thread_id+1)-1;
-int bi = offset*(2*thread_id+2)-1;
-x[bi] += x[ai];
-}
-offset *= 2;
-if (thread_id == 0) {
-x[n - 1] = 0; // clear the last element
-}
-for (int d_down_sweep = 1; d_down_sweep < n; d_down_sweep *= 2) {  // traverse down tree & build scan
-offset >>= 1;
-__syncthreads();
-if (thread_id < d_down_sweep) {
-int ai = offset*(2*thread_id+1)-1;
-int bi = offset*(2*thread_id+2)-1;
-float t = x[ai];
-x[ai] = x[bi];
-x[bi] += t;
-}
-}
-__syncthreads();
-g_odata[2*thread_id] = x[2*thread_id]; // write results to device memory
-g_odata[2*thread_id+1] = x[2*thread_id+1];
-}
+    extern  __shared__  float x[];
+    int thread_id = threadIdx.x;
+    int offset = 1;
+    x[2 * thread_id] = g_idata[2 * thread_id];
+    x[2 * thread_id + 1] = g_idata[2 * thread_id + 1];
+    for (int dr = n >> 1; dr > 0; dr >>= 1) {
+        __syncthreads();
+        if (thread_id < dr) {
+            int ai = offset * (2 * thread_id + 1) - 1;
+            int bi = offset * (2 * thread_id + 2) - 1;
+            x[bi] += x[ai];
+        }
+        if (thread_id == 0) {
+            x[n - 1] = 0;
+        }
+        offset *= 2;
+        for (int ds = 1; ds < n; ds *= 2) {
+            offset >>= 1;
+            __syncthreads();
+            if (thread_id < ds) {
+                int ai = offset * (2 * thread_id + 1) - 1;
+                int bi = offset * (2 * thread_id + 2) - 1;
+                float t = x[ai];
+                x[ai] = x[bi];
+                x[bi] += t;
+            }
+        }
+        __syncthreads();
+        g_odata[2 * thread_id] = x[2 * thread_id];
+        g_odata[2 * thread_id+1] = x[2 * thread_id + 1];
+    }
 }
 
 /*
